7075873146c9f5ddcf4d3eeb6a8b18928f9a52ac
Jeremy Drake <github@jdrake.com>
2024-06-05 17:04:21 -0700

    Update msys32.py
    New version.

--- examples/msys32.py
+++ examples/msys32.py
@@ -4,14 +4,9 @@ import pacdb
 
 build32 = pacdb.Database.from_url('build32', 'https://github.com/jeremyd2019/msys2-build32/releases/download/repo')
 msys64 = pacdb.msys_db_by_arch('x86_64')
-msys32 = pacdb.msys_db_by_arch('i686')
-
-
-combined32 = {pkg.name: pkg for pkg in msys32}
-combined32.update({pkg.name: pkg for pkg in build32})
 
 pkgs64 = {str(pkg): pkg for pkg in msys64}
-pkgs32 = {str(pkg): pkg for pkg in combined32.values()}
+pkgs32 = {str(pkg): pkg for pkg in build32}
 
 updates = pkgs64.keys() - pkgs32.keys()
 
